5b94eb1682ce7c32b25d69bde10441651d1b05c2
Hemant Bhatnagar <hemantbhatnagar0005@gmail.com>
2024-07-28 02:14:43 +0530

    Create Patch File

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -23,16 +23,16 @@ interface PerspectiveViewerElement {
  * parsed from its parent through data property.
  */
 class Graph extends Component<IProps, {}> {
-  // Perspective table
   table: Table | undefined;
 
   render() {
-    return React.createElement('perspective-viewer');
+    return React.createElement('perspective-viewer', { id: 'perspective-viewer' });
   }
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementById('perspective-viewer') as unknown as PerspectiveViewerElement;
+
 
     const schema = {
       stock: 'string',
@@ -40,15 +40,21 @@ class Graph extends Component<IProps, {}> {
       top_bid_price: 'float',
       timestamp: 'date',
     };
-
-    if (window.perspective && window.perspective.worker()) {
+    if (window.perspective) {
       this.table = window.perspective.worker().table(schema);
-    }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-      // Add more Perspective configurations here.
-      elem.load(this.table);
+      if (this.table && elem) {
+        elem.load(this.table);
+        elem.setAttribute('view', 'y_line');
+        elem.setAttribute('column-pivots', '["stock"]');
+        elem.setAttribute('row-pivots', '["timestamp"]');
+        elem.setAttribute('columns', '["top_ask_price"]');
+        elem.setAttribute('aggregates', JSON.stringify({
+          stock: 'distinct count',
+          top_ask_price: 'avg',
+          top_bid_price: 'avg',
+          timestamp: 'distinct count'
+        }));
+      }
     }
   }
 
